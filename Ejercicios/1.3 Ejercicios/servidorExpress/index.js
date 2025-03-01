b365f719d6d3e37562a664f34d59647959712e19
Alexisgmz0208 <alexiselcrack5@gmail.com>
2025-02-28 23:47:01 -0500

    aplicacion servidorExpress mejorado

--- Ejercicios/1.3 Ejercicios/servidorExpress/index.js
+++ Ejercicios/1.3 Ejercicios/servidorExpress/index.js
@@ -1,33 +1,14 @@
-// import express from "express";
-// import path from "path";
+import express from "express";
+import path from "path";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 
-// const app = express();
-// const port = 3000;
-
-// // Rutas
-// app.get("/", (req, res  )=>{
-//     res.sendFile(path.join(__dirname,'./views/home.html'));
-// });
-
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname,'./views/about.html'));
-// });
-
-// app.get('/contact', (req, res) => {
-//     res.sendFile(path.join(__dirname,'./views/contact.html'));
-// });
-
-// app.listen(port, () => {
-//     console.log(`Servidor escuchando en http://localhost:${port}`);
-
-const express = require('express');
-const path = require('path');
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static('views'));
+app.use(express.static('public'));
 
 // Rutas
 app.get('/', (req, res) => {
